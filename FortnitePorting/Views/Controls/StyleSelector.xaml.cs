2124bee0b2287b1888271433361ea4ab6661e20b
halfuwu <maxman1346@gmail.com>
2022-10-27 13:40:21 -0400

    fallback for empty stw styles

--- FortnitePorting/Views/Controls/StyleSelector.xaml.cs
+++ FortnitePorting/Views/Controls/StyleSelector.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Controls;
 using CUE4Parse_Conversion.Textures;
+using CUE4Parse.UE4.Assets.Exports;
 using CUE4Parse.UE4.Assets.Exports.Texture;
 using CUE4Parse.UE4.Assets.Objects;
 using SkiaSharp;
@@ -12,7 +13,7 @@ public partial class StyleSelector
     public string ChannelName;
     
     
-    public StyleSelector(string channelName, FStructFallback[] options)
+    public StyleSelector(string channelName, FStructFallback[] options, SKBitmap fallbackBitmap)
     {
         InitializeComponent();
         DataContext = this;
@@ -20,9 +21,12 @@ public partial class StyleSelector
         ChannelName = channelName;
         foreach (var option in options)
         {
-            if (!option.TryGetValue(out UTexture2D previewTexture, "PreviewImage")) return;
-            var previewBitmap = previewTexture.Decode();
-            if (previewBitmap is null) continue;
+            var previewBitmap = fallbackBitmap;
+            if (option.TryGetValue(out UTexture2D previewTexture, "PreviewImage"))
+            {
+                previewBitmap = previewTexture.Decode();
+                if (previewBitmap is null) continue;
+            }
             
             var fullBitmap = new SKBitmap(previewBitmap.Width, previewBitmap.Height, previewBitmap.ColorType, previewBitmap.AlphaType);
             using (var fullCanvas = new SKCanvas(fullBitmap))
