f7680ee19a80a67ece1333604f31cc92487d1a6d
halfuwu <maxman1346@gmail.com>
2022-12-18 14:43:31 -0500

    support for tag driven cosmetic styles

--- FortnitePorting/Exports/Types/MeshExportData.cs
+++ FortnitePorting/Exports/Types/MeshExportData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using CUE4Parse.GameTypes.FN.Assets.Exports;
 using CUE4Parse.UE4.Assets.Exports;
@@ -8,6 +9,8 @@ using CUE4Parse.UE4.Assets.Exports.StaticMesh;
 using CUE4Parse.UE4.Assets.Objects;
 using CUE4Parse.UE4.Objects.Core.i18N;
 using CUE4Parse.UE4.Objects.Engine;
+using CUE4Parse.UE4.Objects.GameplayTags;
+using CUE4Parse.UE4.Objects.UObject;
 using FortnitePorting.AppUtils;
 
 namespace FortnitePorting.Exports.Types;
@@ -87,18 +90,70 @@ public class MeshExportData : ExportDataBase
 
             return true;
         });
-
         if (!canContinue) return null;
-
-        foreach (var style in styles)
-        {
-            ExportHelpers.CharacterParts(style.GetOrDefault("VariantParts", Array.Empty<UObject>()), data.StyleParts);
-            ExportHelpers.OverrideMaterials(style.GetOrDefault("VariantMaterials", Array.Empty<FStructFallback>()), data.StyleMaterials);
-            ExportHelpers.OverrideMeshes(style.GetOrDefault("VariantMeshes", Array.Empty<FStructFallback>()), data.StyleMeshes);
-            // TODO VARIANT PARAMS
-        }
+        
+        data.ProcessStyles(asset, styles);
 
         await Task.WhenAll(ExportHelpers.Tasks);
         return data;
+    }
+    
+}
+
+public static class MeshExportExtensions
+{
+    public static void ProcessStyles(this MeshExportData data, UObject asset, FStructFallback[] selectedStyles)
+    {
+        var totalMetaTags = new List<string>();
+        var metaTagsToApply = new List<string>();
+        var metaTagsToRemove= new List<string>();
+        foreach (var style in selectedStyles)
+        {
+            var tags = style.Get<FStructFallback>("MetaTags");
+            
+            var tagstoApply = tags.Get<FGameplayTagContainer>("MetaTagsToApply");
+            metaTagsToApply.AddRange(tagstoApply.GameplayTags.Select(x => x.Text));
+            
+            var tagsToRemove = tags.Get<FGameplayTagContainer>("MetaTagsToRemove");
+            metaTagsToRemove.AddRange(tagsToRemove.GameplayTags.Select(x => x.Text));
+        }
+        
+        totalMetaTags.AddRange(metaTagsToApply);
+        metaTagsToRemove.ForEach(tag => totalMetaTags.RemoveAll(x => x.Equals(tag, StringComparison.OrdinalIgnoreCase)));
+        
+        var itemStyles = asset.GetOrDefault("ItemVariants", Array.Empty<UObject>());
+        var tagDrivenStyles = itemStyles.Where(style => style.ExportType.Equals("FortCosmeticLoadoutTagDrivenVariant"));
+        foreach (var tagDrivenStyle in tagDrivenStyles)
+        {
+            var options = tagDrivenStyle.Get<FStructFallback[]>("Variants");
+            foreach (var option in options)
+            {
+                var requiredConditions = option.Get<FStructFallback[]>("RequiredConditions");
+                foreach (var condition in requiredConditions)
+                {
+                    var metaTagQuery = condition.Get<FStructFallback>("MetaTagQuery");
+                    var tagDictionary = metaTagQuery.Get<FStructFallback[]>("TagDictionary");
+                    var requiredTags = tagDictionary.Select(x => x.Get<FName>("TagName").Text).ToList();
+                    if (requiredTags.All(x => totalMetaTags.Contains(x)))
+                    {
+                        ExportStyleData(option, data);
+                    }
+                }
+            }
+        }
+
+        foreach (var style in selectedStyles)
+        {
+            ExportStyleData(style, data);
+        }
+        
+    }
+
+    private static void ExportStyleData(FStructFallback style, MeshExportData data)
+    {
+        ExportHelpers.CharacterParts(style.GetOrDefault("VariantParts", Array.Empty<UObject>()), data.StyleParts);
+        ExportHelpers.OverrideMaterials(style.GetOrDefault("VariantMaterials", Array.Empty<FStructFallback>()), data.StyleMaterials);
+        ExportHelpers.OverrideMeshes(style.GetOrDefault("VariantMeshes", Array.Empty<FStructFallback>()), data.StyleMeshes);
+        // TODO VARIANT PARAMS
     }
 }
