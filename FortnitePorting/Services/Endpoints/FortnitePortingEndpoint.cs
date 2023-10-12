0ee67baa266e38364dd173ed92c3ed1a0e2897b6
halfuwu <maxman1346@gmail.com>
2023-10-12 16:29:51 -0400

    scuffed fallback fix

--- FortnitePorting/Services/Endpoints/FortnitePortingEndpoint.cs
+++ FortnitePorting/Services/Endpoints/FortnitePortingEndpoint.cs
@@ -6,16 +6,20 @@ namespace FortnitePorting.Services.Endpoints;
 
 public class FortnitePortingEndpoint : EndpointBase
 {
+    private const string NORMAL_DOMAIN = "https://halfheart.dev/fortnite-porting/api/v1/";
+    private const string FALLBACK_DOMAIN = "https://raw.githubusercontent.com/halfuwu/halfheart.dev/master/docs/fortnite-porting/api/v1/";
+    
     public FortnitePortingEndpoint(RestClient client) : base(client)
     {
     }
 
-    public async Task<UpdateInfo?> GetReleaseInfoAsync()
+    public async Task<UpdateInfo?> GetReleaseInfoAsync(string domain = NORMAL_DOMAIN)
     {
-        var request = new RestRequest($"https://halfheart.dev/fortnite-porting/api/v1/release.json");
+        var request = new RestRequest(domain + "release.json");
         var response = await _client.ExecuteAsync<UpdateInfo>(request).ConfigureAwait(false);
         Log.Information("[{Method}] {StatusDescription} ({StatusCode}): {URI}", request.Method, response.StatusDescription, (int) response.StatusCode, request.Resource);
-        return response.Data;
+        if (domain == FALLBACK_DOMAIN) return response.Data;
+        return response.Data ?? await GetReleaseInfoAsync(FALLBACK_DOMAIN);
     }
 
     public UpdateInfo? GetReleaseInfo()
@@ -23,12 +27,13 @@ public class FortnitePortingEndpoint : EndpointBase
         return GetReleaseInfoAsync().GetAwaiter().GetResult();
     }
 
-    public async Task<Broadcast[]?> GetBroadcastsAsync()
+    public async Task<Broadcast[]?> GetBroadcastsAsync(string domain = NORMAL_DOMAIN)
     {
-        var request = new RestRequest($"https://halfheart.dev/fortnite-porting/api/v1/broadcast.json");
+        var request = new RestRequest(domain + "broadcast.json");
         var response = await _client.ExecuteAsync<Broadcast[]>(request).ConfigureAwait(false);
         Log.Information("[{Method}] {StatusDescription} ({StatusCode}): {URI}", request.Method, response.StatusDescription, (int) response.StatusCode, request.Resource);
-        return response.Data;
+        if (domain == FALLBACK_DOMAIN) return response.Data;
+        return response.Data ?? await GetBroadcastsAsync(FALLBACK_DOMAIN);
     }
 
     public Broadcast[]? GetBroadcasts()
@@ -36,12 +41,13 @@ public class FortnitePortingEndpoint : EndpointBase
         return GetBroadcastsAsync().GetAwaiter().GetResult();
     }
 
-    public async Task<BackupAPI?> GetBackupAsync()
+    public async Task<BackupAPI?> GetBackupAsync(string domain = NORMAL_DOMAIN)
     {
-        var request = new RestRequest($"https://halfheart.dev/fortnite-porting/api/v1/backup.json");
+        var request = new RestRequest(domain + "backup.json");
         var response = await _client.ExecuteAsync<BackupAPI>(request).ConfigureAwait(false);
         Log.Information("[{Method}] {StatusDescription} ({StatusCode}): {URI}", request.Method, response.StatusDescription, (int) response.StatusCode, request.Resource);
-        return response.Data;
+        if (domain == FALLBACK_DOMAIN) return response.Data;
+        return response.Data ?? await GetBackupAsync(FALLBACK_DOMAIN);
     }
 
     public BackupAPI? GetBackup()
