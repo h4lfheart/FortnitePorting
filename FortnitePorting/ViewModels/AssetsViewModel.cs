fd9bea98a88fa62533cb152f98155f2125aed098
halfuwu <maxman1346@gmail.com>
2024-04-30 23:11:21 -0400

    sort icon based on ascending/descending toggle

--- FortnitePorting/ViewModels/AssetsViewModel.cs
+++ FortnitePorting/ViewModels/AssetsViewModel.cs
@@ -1,12 +1,18 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using FortnitePorting.Shared;
 using FortnitePorting.Shared.Framework;
+using Material.Icons;
 
 namespace FortnitePorting.ViewModels;
 
 public partial class AssetsViewModel : ViewModelBase
 {
-    [ObservableProperty] private EAssetSortType _sortType = EAssetSortType.None;
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(SortIcon))]
+    private bool _descendingSort = false;
+
+    public MaterialIconKind SortIcon => DescendingSort ? MaterialIconKind.SortDescending : MaterialIconKind.SortAscending;
     
+    [ObservableProperty] private EAssetSortType _sortType = EAssetSortType.None;
     [ObservableProperty] private EExportLocation _exportLocation = EExportLocation.Blender;
 }
