da82c06761113ba10283414c3b39f6bae6aea545
Benjamin Marquez <bmarquez1997@gmail.com>
2024-12-15 09:40:01 -0500

    Updated Live API token

--- FortnitePorting/Plugins/Blender/FortnitePorting/__init__.py
+++ FortnitePorting/Plugins/Blender/FortnitePorting/__init__.py
@@ -10,7 +10,7 @@ bl_info = {
     "description": "Fortnite Porting Blender Plugin",
     "author": "Half",
     "blender": (4, 0, 0),
-    "version": (2, 2, 4),
+    "version": (2, 2, 5),
     "category": "Import",
 }
 
