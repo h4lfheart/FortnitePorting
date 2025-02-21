0794dcb87bf76156cce925175fcd6a7b95205518
halfuwu <maxman1346@gmail.com>
2025-02-21 14:59:21 -0500

    clean up plugin system

---
+++ FortnitePorting/Models/Plugin/BlenderInstallation.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using FortnitePorting.Shared.Services;
+using Newtonsoft.Json;
+using Serilog;
+using Tomlyn;
+
+namespace FortnitePorting.Models.Plugin;
+
+public partial class BlenderInstallation(string blenderExecutablePath) : ObservableObject
+{
+    [ObservableProperty] private string _blenderPath = blenderExecutablePath;
+    
+    [ObservableProperty] 
+    [field: JsonIgnore]
+    private Version? _extensionVersion = null;
+    
+    [ObservableProperty]
+    [field: JsonIgnore]
+    private string _status = string.Empty;
+
+    public Version BlenderVersion => GetVersion(BlenderPath);
+    private string ManifestPath => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), 
+        "Blender Foundation", 
+        "Blender", 
+        BlenderVersion.ToString(2), 
+        "extensions", 
+        "user_default",
+        "fortnite_porting",
+        "blender_manifest.toml");
+    
+    public static readonly DirectoryInfo PluginWorkingDirectory = new(Path.Combine(PluginsFolder.FullName, "Blender"));
+    public static readonly Version MinimumVersion = new(4, 2);
+
+    public static Version GetVersion(string blenderPath)
+    {
+        return new Version(FileVersionInfo.GetVersionInfo(blenderPath).ProductVersion!);
+    }
+
+    public bool SyncExtensionVersion()
+    {
+        if (!File.Exists(ManifestPath))
+        {
+            AppWM.Message("Blender Extension", $"Plugin manifest does not exist at path {ManifestPath}, installation may have gone wrong.\nPlease remove the installation from Blender and Fortnite Porting and try again.");
+            return false;
+        }
+        
+        var manifestContents = File.ReadAllText(ManifestPath);
+        var manifestToml = Toml.ToModel(manifestContents);
+        ExtensionVersion = new Version((string) manifestToml["version"]);
+        return true;
+    }
+    
+    public void Install()
+    {
+        Status = "Installing";
+        
+        var ueFormatZip = BuildPlugin("io_scene_ueformat");
+        var fnPortingZip = BuildPlugin("fortnite_porting");
+
+        InstallPlugin(ueFormatZip);
+        InstallPlugin(fnPortingZip);
+
+        SyncExtensionVersion();
+        
+        Status = string.Empty;
+    }
+
+    public void Uninstall()
+    {
+        Status = "Uninstalling";
+        RemovePlugin("fortnite_porting");
+        RemovePlugin("io_scene_ueformat");
+    }
+
+    private string BuildPlugin(string name)
+    {
+        Status = $"Building {name}";
+        
+        var outPath = Path.Combine(PluginWorkingDirectory.FullName, $"{name}.zip");
+        BlenderExtensionCommand("build", $"--output-filepath \"{outPath}\" --verbose", workingDirectory: Path.Combine(PluginWorkingDirectory.FullName, name));
+        return outPath;
+    }
+
+    private void InstallPlugin(string zipPath)
+    {
+        Status = $"Installing {Path.GetFileName(zipPath)}";
+        BlenderExtensionCommand("install-file", $"\"{zipPath}\" -r user_default -e");
+    }
+
+    private void RemovePlugin(string name)
+    {
+        BlenderExtensionCommand("remove", name);
+    }
+
+    private void BlenderExtensionCommand(string command, string args, string workingDirectory = "")
+    {
+        using var buildProcess = new Process();
+        buildProcess.StartInfo = new ProcessStartInfo
+        {
+            FileName = BlenderPath,
+            Arguments = $"--command extension {command} {args}",
+            WorkingDirectory = workingDirectory,
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        Log.Information($"Executing {BlenderPath} {command} {args}");
+        buildProcess.Start();
+        buildProcess.WaitForExit();
+        
+        var output = buildProcess.StandardOutput.ReadToEnd();
+        var lockMatch = Regex.Match(output, "Error: Lock exists: lock is held by other session: (.*)");
+        if (lockMatch.Groups.Count > 1)
+        {
+            AppWM.Message("Blender Extension", $"A lock has been put on the fortnite_porting extension repository. Please delete \"{lockMatch.Groups[1].Value.Trim()}\" and try again.");
+        }
+    }
+    
+}
--- FortnitePorting/ViewModels/Plugin/BlenderPluginViewModel.cs
+++ FortnitePorting/ViewModels/Plugin/BlenderPluginViewModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -24,58 +25,64 @@ namespace FortnitePorting.ViewModels.Plugin;
 public partial class BlenderPluginViewModel : ViewModelBase
 {
     [ObservableProperty] private bool _automaticallySync = true;
-    [ObservableProperty] private ObservableCollection<BlenderInstallationInfo> _installations = [];
+    [ObservableProperty] private ObservableCollection<BlenderInstallation> _installations = [];
     [ObservableProperty, JsonIgnore] private int _selectedInstallationIndex = 0;
-
-    private static readonly DirectoryInfo BlenderRoot = new(Path.Combine(PluginsFolder.FullName, "Blender"));
-    private static readonly Version MinimumVersion = new(4, 2);
 
     public override async Task Initialize()
     {
-        if (!BlenderRoot.Exists)
-            BlenderRoot.Create();
+        if (!BlenderInstallation.PluginWorkingDirectory.Exists)
+            BlenderInstallation.PluginWorkingDirectory.Create();
 
-        SyncExtensionVersions();
-
-    }
-
-    public void SyncExtensionVersions()
-    {
         foreach (var installation in Installations.ToArray())
         {
-            if (!installation.SyncExtensionVersion())
-            {
-                Installations.Remove(installation);
-            }
+            if (installation.SyncExtensionVersion()) continue;
+            
+            installation.Uninstall();
+            Installations.Remove(installation);
         }
     }
+
 
     public async Task AddInstallation()
     {
         if (await BrowseFileDialog(fileTypes: Globals.BlenderFileType) is not { } blenderPath) return;
 
-        if (TryGetBlenderProcess(blenderPath, out var blenderProcess))
+        var blenderVersion = BlenderInstallation.GetVersion(blenderPath);
+        if (Installations.Any(existing => existing.BlenderVersion == blenderVersion))
         {
-            AppWM.Message("Failed to Add Blender Installation", 
-                $"This version of blender is currently open. Please close it and re-add the installation.\nPath: {blenderPath}\nPID: {blenderProcess.Id}", 
-                InfoBarSeverity.Error, autoClose: false);
+            AppWM.Message("Blender Extension", $"The plugin for Blender {blenderVersion} has already been installed.", InfoBarSeverity.Warning);
             return;
         }
-
-        var installationInfo = new BlenderInstallationInfo(blenderPath);
-        if (Installations.Any(installation => installation.BlenderVersion == installationInfo.BlenderVersion)) return;
-        if (installationInfo.BlenderVersion < MinimumVersion)
+        
+        if (blenderVersion < BlenderInstallation.MinimumVersion)
         {
             AppWM.Message("Blender Extension", 
-                $"Blender {installationInfo.BlenderVersion} is too low of a version. Only Blender {MinimumVersion} and higher are supported.", 
+                $"Blender {blenderVersion} is too low of a version. Only Blender {BlenderInstallation.MinimumVersion} and higher are supported.", 
                 InfoBarSeverity.Error, autoClose: false);
             return;
         }
         
-        Installations.Add(installationInfo);
+        if (TryGetBlenderProcess(blenderPath, out var blenderProcess))
+        {
+            AppWM.Message("Failed to Add Blender Installation", 
+                $"This version of blender is currently open. Please close it and re-add the installation.", 
+                InfoBarSeverity.Error, autoClose: false, 
+                useButton: true, buttonTitle: "Kill Blender Process", buttonCommand: () =>
+                {
+                    blenderProcess.Kill(entireProcessTree: true);
+                });
+            return;
+        }
+
+        var installation = new BlenderInstallation(blenderPath);
+        
+        Installations.Add(installation);
         SelectedInstallationIndex = Installations.Count - 1;
 
-        await Sync(installationInfo);
+        await TaskService.RunAsync(() =>
+        {
+            installation.Install();
+        });
     }
 
     public async Task RemoveInstallation()
@@ -84,19 +91,12 @@ public partial class BlenderPluginViewModel : ViewModelBase
         
         await TaskService.RunAsync(() =>
         {
-            var installation = Installations[SelectedInstallationIndex];
-            installation.Status = "Removing";
-            
-            if (File.Exists(installation.BlenderPath)) {
-                
-                RemovePlugin("io_scene_ueformat", installation.BlenderPath);
-                RemovePlugin("fortnite_porting", installation.BlenderPath);
-            }
-
-            var selectedIndexToRemove = SelectedInstallationIndex;
-            Installations.RemoveAt(selectedIndexToRemove);
-            SelectedInstallationIndex = selectedIndexToRemove == 0 ? 0 : selectedIndexToRemove - 1;
+            Installations[SelectedInstallationIndex].Uninstall();
         });
+        
+        var selectedIndexToRemove = SelectedInstallationIndex;
+        Installations.RemoveAt(selectedIndexToRemove);
+        SelectedInstallationIndex = selectedIndexToRemove == 0 ? 0 : selectedIndexToRemove - 1;
     }
 
     public async Task SyncInstallations()
@@ -133,78 +133,16 @@ public partial class BlenderPluginViewModel : ViewModelBase
             }
 
             var previousVersion = installation.ExtensionVersion;
-            await Sync(installation);
+            installation.Install();
 
             AppWM.Message("Blender Extension", $"Successfully updated the Blender {installation.BlenderVersion} extension from {previousVersion} to {currentVersion}");
         }
     }
-    public async Task Sync(BlenderInstallationInfo installation)
-    {
-        installation.Status = "Syncing";
-        
-        await TaskService.Run(() =>
-        {
-            var ueFormatZip = BuildPlugin(installation, "io_scene_ueformat", installation.BlenderPath);
-            var fnPortingZip = BuildPlugin(installation, "fortnite_porting", installation.BlenderPath);
 
-            InstallPlugin(installation, ueFormatZip, installation.BlenderPath);
-            InstallPlugin(installation, fnPortingZip, installation.BlenderPath);
-
-            installation.SyncExtensionVersion();
-        });
-        
-        installation.Status = string.Empty;
-    }
-
-    public string BuildPlugin(BlenderInstallationInfo installation, string name, string blenderPath)
-    {
-        installation.Status = $"Building {name}";
-        
-        var outPath = Path.Combine(BlenderRoot.FullName, $"{name}.zip");
-        BlenderExtensionCommand("build", $"--output-filepath \"{outPath}\"", blenderPath,
-            workingDirectory: Path.Combine(BlenderRoot.FullName, name));
-        return outPath;
-    }
-    
-    public void InstallPlugin(BlenderInstallationInfo installation, string zipPath, string blenderPath)
-    {
-        installation.Status = $"Installing {Path.GetFileName(zipPath)}";
-        BlenderExtensionCommand("install-file", $"\"{zipPath}\" -r user_default -e", blenderPath);
-    }
-    
-    public void RemovePlugin(string name, string blenderPath)
-    {
-        BlenderExtensionCommand("remove", name, blenderPath);
-    }
-
-    public void BlenderExtensionCommand(string command, string args, string blenderPath, string workingDirectory = "")
-    {
-        using var buildProcess = new Process();
-        buildProcess.StartInfo = new ProcessStartInfo
-        {
-            FileName = blenderPath,
-            Arguments = $"--command extension {command} {args}",
-            WorkingDirectory = workingDirectory,
-            UseShellExecute = true
-        };
-
-        Log.Information($"Executing {blenderPath} {command} {args}");
-        buildProcess.Start();
-        var output = string.Empty;// buildProcess.StandardOutput.ReadToEnd();
-        buildProcess.WaitForExit();
-        
-        var lockMatch = Regex.Match(output, "Error: Lock exists: lock is held by other session: (.*)");
-        if (lockMatch.Groups.Count > 1)
-        {
-            AppWM.Message("Blender Extension", $"A lock has been put on the user_default extension repository. Please delete \"{lockMatch.Groups[1].Value.Trim()}\" and try again.");
-        }
-    }
-
-    public bool TryGetBlenderProcess(string path, out Process process)
+    private static bool TryGetBlenderProcess(string path, [MaybeNullWhen(false)] out Process process)
     {
         var blenderProcesses = Process.GetProcessesByName("blender");
         process = blenderProcesses.FirstOrDefault(process => process.MainModule is { } mainModule && mainModule.FileName.Equals(path.Replace("/", "\\")));
         return process is not null;
-
     }
 }
