2bbd6ac5cfadc9d515439bf851d36a226f10af09
Benjamin Marquez <bmarquez1997@gmail.com>
2025-04-16 17:04:17 -0400

    UE 5.5 and UEFormat 8 update

--- FortnitePorting.Plugins/Unreal/UEFormat/Source/UEFormat/Private/Readers/UEFAnimReader.cpp
+++ FortnitePorting.Plugins/Unreal/UEFormat/Source/UEFormat/Private/Readers/UEFAnimReader.cpp
@@ -1,9 +1,19 @@
-﻿#include "Readers/UEFAnimReader.h"
-#include "Misc/Compression.h"
+﻿// Copyright © 2025 Marcel K. All rights reserved.
+
+#include "Readers/UEFAnimReader.h"
 #include "Readers/UEFModelReader.h"
+#include "Misc/Compression.h"
+#include <string>
+#include "zstd.h"
 
 UEFAnimReader::UEFAnimReader(const FString Filename) {
 	Ar.open(ToCStr(Filename), std::ios::binary);
+}
+
+UEFAnimReader::~UEFAnimReader() { //Destructor
+	if (Ar.is_open()) {
+		Ar.close();
+	}
 }
 
 bool UEFAnimReader::Read() {
@@ -14,65 +24,115 @@ bool UEFAnimReader::Read() {
 	Header.FileVersionBytes = ReadData<std::byte>(Ar);
 	Header.ObjectName = ReadFString(Ar);
 	Header.IsCompressed = ReadData<bool>(Ar);
-	NumFrames = ReadData<int32>(Ar);
-	FramesPerSecond = ReadData<float>(Ar);
 
-	ReadArchive(Ar);
+	if (Header.IsCompressed) {
+		Header.CompressionType = ReadFString(Ar);
+		Header.UncompressedSize = ReadData<int32>(Ar);
+		Header.CompressedSize = ReadData<int32>(Ar);
+
+		std::vector<char> CompressedBuffer(Header.CompressedSize);
+		Ar.read(CompressedBuffer.data(), Header.CompressedSize);
+		if (Ar.fail()) {
+			UE_LOG(LogTemp, Error, TEXT("Error reading compressed data."));
+			return false;
+		}
+
+		std::vector<char> UncompressedBuffer(Header.UncompressedSize);
+
+		if (Header.CompressionType == "ZSTD")
+			ZSTD_decompress(UncompressedBuffer.data(), Header.UncompressedSize, CompressedBuffer.data(), Header.CompressedSize);
+
+		else if (Header.CompressionType == "GZIP")
+			FCompression::UncompressMemory(NAME_Gzip, UncompressedBuffer.data(), Header.UncompressedSize, CompressedBuffer.data(), Header.CompressedSize);
+
+		ReadBuffer(UncompressedBuffer.data(), Header.UncompressedSize);
+	}
+	else {
+		const auto CurrentPos = Ar.tellg();
+		Ar.seekg(0, std::ios::end);
+		const auto RemainingSize = Ar.tellg() - CurrentPos;
+		Ar.seekg(CurrentPos, std::ios::beg);
+        
+		std::vector<char> UncompressedBuffer(RemainingSize);
+		Ar.read(UncompressedBuffer.data(), RemainingSize);
+		if (Ar.fail()) {
+			UE_LOG(LogTemp, Error, TEXT("Error reading uncompressed data."));
+			return false;
+		}
+
+		ReadBuffer(UncompressedBuffer.data(), RemainingSize);
+	}
+
+	Ar.close();
 	return true;
 }
 
-void UEFAnimReader::ReadArchive(std::ifstream& Archive)
+void UEFAnimReader::ReadBuffer(const char* Buffer, int32 BufferSize)
 {
-	while (!Archive.eof()) {
-		std::string ChunkName = ReadFString(Archive);
-		const int32 ArraySize = ReadData<int32>(Archive);
-		const int32 ByteSize = ReadData<int32>(Archive);
+	int32 Offset = 0;
 
-		if (ChunkName == "TRACKS")
+	while (Offset < BufferSize)
+	{
+		std::string ChunkName = ReadBufferFString(Buffer, Offset);
+		int32 ArraySize = ReadBufferData<int32>(Buffer, Offset);
+		int32 ByteSize = ReadBufferData<int32>(Buffer, Offset);
+		if (ChunkName == "METADATA")
+		{
+			NumFrames = ReadBufferData<int32>(Buffer, Offset);
+			FramesPerSecond = ReadBufferData<float>(Buffer, Offset);
+			RefPosePath = ReadBufferFString(Buffer, Offset);
+			AdditiveAnimType = static_cast<EAdditiveAnimationType>(ReadBufferData<uint8>(Buffer, Offset));
+			RefPoseType = static_cast<EAdditiveBasePoseType>(ReadBufferData<uint8>(Buffer, Offset));
+			RefFrameIndex = ReadBufferData<int32>(Buffer, Offset);
+		}
+		else if (ChunkName == "TRACKS")
 		{
 			Tracks.SetNum(ArraySize);
-			for (auto i = 0; i < ArraySize; i++) {
-				Tracks[i].TrackName = ReadFString(Archive);
+			for (auto i = 0; i < ArraySize; i++)
+			{
+				Tracks[i].TrackName = ReadBufferFString(Buffer, Offset);
 
-				const int32 PosArraySize = ReadData<int32>(Archive);
+				const int32 PosArraySize = ReadBufferData<int32>(Buffer, Offset);
 				Tracks[i].TrackPosKeys.SetNum(PosArraySize);
-				for (auto j = 0; j < PosArraySize; j++) {
-					Tracks[i].TrackPosKeys[j].Frame = ReadData<int32>(Archive);
-					Tracks[i].TrackPosKeys[j].VectorValue = ReadData<FVector3f>(Archive);
+				for (auto j = 0; j < PosArraySize; j++)
+				{
+					Tracks[i].TrackPosKeys[j].Frame = ReadBufferData<int32>(Buffer, Offset);
+					Tracks[i].TrackPosKeys[j].VectorValue = ReadBufferData<FVector3f>(Buffer, Offset);
 				}
 
-				const int32 RotArraySize = ReadData<int32>(Archive);
+				const int32 RotArraySize = ReadBufferData<int32>(Buffer, Offset);
 				Tracks[i].TrackRotKeys.SetNum(RotArraySize);
-				for (auto k = 0; k < RotArraySize; k++) {
-					Tracks[i].TrackRotKeys[k].Frame = ReadData<int32>(Archive);
-					Tracks[i].TrackRotKeys[k].QuatValue = ReadQuat(Archive);
+				for (auto k = 0; k < RotArraySize; k++)
+				{
+					Tracks[i].TrackRotKeys[k].Frame = ReadBufferData<int32>(Buffer, Offset);
+					Tracks[i].TrackRotKeys[k].QuatValue = ReadBufferQuat(Buffer, Offset);
 				}
 
-				const int32 ScaleArraySize = ReadData<int32>(Archive);
+				const int32 ScaleArraySize = ReadBufferData<int32>(Buffer, Offset);
 				Tracks[i].TrackScaleKeys.SetNum(ScaleArraySize);
-				for (auto l = 0; l < ScaleArraySize; l++) {
-					Tracks[i].TrackScaleKeys[l].Frame = ReadData<int32>(Archive);
-					Tracks[i].TrackScaleKeys[l].VectorValue = ReadData<FVector3f>(Archive);
+				for (auto l = 0; l < ScaleArraySize; l++)
+				{
+					Tracks[i].TrackScaleKeys[l].Frame = ReadBufferData<int32>(Buffer, Offset);
+					Tracks[i].TrackScaleKeys[l].VectorValue = ReadBufferData<FVector3f>(Buffer, Offset);
 				}
 			}
 		}
 		else if (ChunkName == "CURVES")
 		{
 			Curves.SetNum(ArraySize);
-			for (auto i = 0; i < ArraySize; i++) {
-				Curves[i].CurveName = ReadFString(Archive);
-				const int32 KeyArraySize = ReadData<int32>(Archive);
+			for (auto i = 0; i < ArraySize; i++)
+			{
+				Curves[i].CurveName = ReadBufferFString(Buffer, Offset);
+				const int32 KeyArraySize = ReadBufferData<int32>(Buffer, Offset);
 				Curves[i].CurveKeys.SetNum(KeyArraySize);
-				for (auto j = 0; j < KeyArraySize; j++) {
-					Curves[i].CurveKeys[j].Frame = ReadData<int32>(Archive);
-					Curves[i].CurveKeys[j].FloatValue = ReadData<float>(Archive);
+				for (auto j = 0; j < KeyArraySize; j++)
+				{
+					Curves[i].CurveKeys[j].Frame = ReadBufferData<int32>(Buffer, Offset);
+					Curves[i].CurveKeys[j].FloatValue = ReadBufferData<float>(Buffer, Offset);
 				}
 			}
 		}
-		else {
-			Archive.ignore(ByteSize);
-		}
+		else
+			Offset += ByteSize;
 	}
-	Archive.close();
 }
-
--- FortnitePorting.Plugins/Unreal/UEFormat/Source/UEFormat/Private/Readers/UEFModelReader.cpp
+++ FortnitePorting.Plugins/Unreal/UEFormat/Source/UEFormat/Private/Readers/UEFModelReader.cpp
@@ -1,25 +1,15 @@
+// Copyright © 2025 Marcel K. All rights reserved.
+
 #include "Readers/UEFModelReader.h"
-
 #include <string>
-
 #include "zstd.h"
 #include "Misc/Compression.h"
-
-FQuat4f ReadQuat(std::ifstream& Ar)
-{
-    float X = ReadData<float>(Ar);
-    float Y = ReadData<float>(Ar);
-    float Z = ReadData<float>(Ar);
-    float W = ReadData<float>(Ar);
-    auto Data = FQuat4f(X, Y, Z, W);
-    return Data;
-}
 
 std::string ReadString(std::ifstream& Ar, int32 Size)
 {
     std::string String;
     String.resize(Size);
-    Ar.read(&String[0], Size);  //Read data directly into string buffer
+    Ar.read(String.data(), Size);
     return String;
 }
 
@@ -28,7 +18,7 @@ std::string ReadFString(std::ifstream& Ar)
     int32 Size = ReadData<int32>(Ar);
     std::string String;
     String.resize(Size);
-    Ar.read(&String[0], Size);
+    Ar.read(String.data(), Size);
     return String;
 }
 
@@ -38,15 +28,14 @@ FQuat4f ReadBufferQuat(const char* DataArray, int& Offset)
     float Y = ReadBufferData<float>(DataArray, Offset);
     float Z = ReadBufferData<float>(DataArray, Offset);
     float W = ReadBufferData<float>(DataArray, Offset);
-    auto Data = FQuat4f(X, Y, Z, W);
-    return Data;
+    return FQuat4f(X, Y, Z, W).GetNormalized(); // Directly return the value
 }
 
 std::string ReadBufferString(const char* DataArray, int& Offset, int32 Size)
 {
     std::string String;
     String.resize(Size);
-    std::memcpy(&String[0], &DataArray[Offset], Size);
+    std::memcpy(String.data(), &DataArray[Offset], Size);
     Offset += Size;
     return String;
 }
@@ -56,13 +45,17 @@ std::string ReadBufferFString(const char* DataArray, int& Offset)
     int32 Size = ReadBufferData<int32>(DataArray, Offset);
     std::string String;
     String.resize(Size);
-    std::memcpy(&String[0], &DataArray[Offset], Size);
+    std::memcpy(String.data(), &DataArray[Offset], Size);
     Offset += Size;
     return String;
 }
 
-UEFModelReader::UEFModelReader(const FString Filename) {
-    Ar.open(ToCStr(Filename), std::ios::binary);
+UEFModelReader::UEFModelReader(const FString Filename) : Ar(ToCStr(Filename), std::ios::binary) {}
+
+UEFModelReader::~UEFModelReader() { //Destructor
+    if (Ar.is_open()) {
+        Ar.close();
+    }
 }
 
 bool UEFModelReader::Read() {
@@ -73,43 +66,45 @@ bool UEFModelReader::Read() {
     Header.FileVersionBytes = ReadData<std::byte>(Ar);
     Header.ObjectName = ReadFString(Ar);
     Header.IsCompressed = ReadData<bool>(Ar);
-	
+
     if (Header.IsCompressed) {
         Header.CompressionType = ReadFString(Ar);
         Header.UncompressedSize = ReadData<int32>(Ar);
-        Header.CompressedSize = ReadData<int32>(Ar);    
-        
-        char* CompressedBuffer = new char[Header.CompressedSize];
-        Ar.read(CompressedBuffer, Header.CompressedSize);
-        
-        char* UncompressedBuffer = new char[Header.UncompressedSize];
-        
+        Header.CompressedSize = ReadData<int32>(Ar);
+
+        std::vector<char> CompressedBuffer(Header.CompressedSize);
+        Ar.read(CompressedBuffer.data(), Header.CompressedSize);
+        if (Ar.fail()) {
+            UE_LOG(LogTemp, Error, TEXT("Error reading compressed data."));
+            return false;
+        }
+
+        std::vector<char> UncompressedBuffer(Header.UncompressedSize);
+
         if (Header.CompressionType == "ZSTD")
-        {
-            ZSTD_decompress(UncompressedBuffer, Header.UncompressedSize, CompressedBuffer, Header.CompressedSize);
-        }
+            ZSTD_decompress(UncompressedBuffer.data(), Header.UncompressedSize, CompressedBuffer.data(), Header.CompressedSize);
+
         else if (Header.CompressionType == "GZIP")
-        {
-            FCompression::UncompressMemory(NAME_Gzip, UncompressedBuffer, Header.UncompressedSize, CompressedBuffer, Header.CompressedSize);
-        }
-        
-        ReadBuffer(UncompressedBuffer, Header.UncompressedSize);
+            FCompression::UncompressMemory(NAME_Gzip, UncompressedBuffer.data(), Header.UncompressedSize, CompressedBuffer.data(), Header.CompressedSize);
+
+        ReadBuffer(UncompressedBuffer.data(), Header.UncompressedSize);
     }
-    else
-    {
-        //find out remaining size
+    else {
         const auto CurrentPos = Ar.tellg();
         Ar.seekg(0, std::ios::end);
-        const auto EndPos = Ar.tellg();
+        const auto RemainingSize = Ar.tellg() - CurrentPos;
         Ar.seekg(CurrentPos, std::ios::beg);
+        
+        std::vector<char> UncompressedBuffer(RemainingSize);
+        Ar.read(UncompressedBuffer.data(), RemainingSize);
+        if (Ar.fail()) {
+            UE_LOG(LogTemp, Error, TEXT("Error reading uncompressed data."));
+            return false;
+        }
 
-        const auto RemainingSize = EndPos - CurrentPos;
-        char* UncompressedBuffer = new char[RemainingSize];
-
-        Ar.read(UncompressedBuffer, RemainingSize);
-        ReadBuffer(UncompressedBuffer, RemainingSize);
-        delete[] UncompressedBuffer;
+        ReadBuffer(UncompressedBuffer.data(), RemainingSize);
     }
+
     Ar.close();
     return true;
 }
@@ -117,29 +112,32 @@ bool UEFModelReader::Read() {
 void UEFModelReader::ReadBuffer(const char* Buffer, int32 BufferSize) {
     int32 Offset = 0;
 
-    while (Offset < BufferSize) {
+    while (Offset < BufferSize)
+    {
         std::string ChunkName = ReadBufferFString(Buffer, Offset);
         int32 ArraySize = ReadBufferData<int32>(Buffer, Offset);
         int32 ByteSize = ReadBufferData<int32>(Buffer, Offset);
 
-        if (ChunkName == "LODS") {
+        if (ChunkName == "LODS")
+        {
             LODs.SetNum(ArraySize);
             for (int32 index = 0; index < ArraySize; ++index) {
                 std::string LODName = ReadBufferFString(Buffer, Offset);
                 int32 LODByteSize = ReadBufferData<int32>(Buffer, Offset);
-                ReadChunks(Buffer, Offset, LODName, 0, LODByteSize, index);
+                ReadChunks(Buffer, Offset, LODByteSize, index);
             }
-        } else if (ChunkName == "SKELETON") {
-            ReadChunks(Buffer, Offset, ChunkName, 0, ByteSize, 0);
-        } else {
-            Offset += ByteSize;
         }
+        else if (ChunkName == "SKELETON")
+            ReadChunks(Buffer, Offset, ByteSize, 0);
+        else
+            Offset += ByteSize;
     }
 }
 
-void UEFModelReader::ReadChunks(const char* Buffer, int32& Offset, const std::string& ChunkName, int32 ArraySize, int32 ByteSize, int32 LODIndex) {
+void UEFModelReader::ReadChunks(const char* Buffer, int32& Offset, int32 ByteSize, int32 LODIndex) {
     int32 InnerOffset = Offset; // Offset for nested data
-    while (InnerOffset < Offset + ByteSize) {
+    while (InnerOffset < Offset + ByteSize)
+    {
         std::string InnerChunkName = ReadBufferFString(Buffer, InnerOffset);
         int32 InnerArraySize = ReadBufferData<int32>(Buffer, InnerOffset);
         int32 InnerByteSize = ReadBufferData<int32>(Buffer, InnerOffset);
@@ -152,72 +150,108 @@ void UEFModelReader::ReadChunks(const char* Buffer, int32& Offset, const std::st
             ReadBufferArray(Buffer, InnerOffset, InnerArraySize, LODs[LODIndex].Normals);
         else if (InnerChunkName == "TANGENTS")
             ReadBufferArray(Buffer, InnerOffset, InnerArraySize, LODs[LODIndex].Tangents);
-        else if (InnerChunkName == "VERTEXCOLORS") {
+        else if (InnerChunkName == "VERTEXCOLORS")
+        {
             LODs[LODIndex].VertexColors.SetNum(InnerArraySize);
-            for (auto i = 0; i < InnerArraySize; i++) {
+            for (auto i = 0; i < InnerArraySize; i++)
+            {
                 LODs[LODIndex].VertexColors[i].Name = ReadBufferFString(Buffer, InnerOffset);
                 LODs[LODIndex].VertexColors[i].Count = ReadBufferData<int32>(Buffer, InnerOffset);
                 LODs[LODIndex].VertexColors[i].Data.SetNum(LODs[LODIndex].VertexColors[i].Count);
                 ReadBufferArray(Buffer, InnerOffset, LODs[LODIndex].VertexColors[i].Count, LODs[LODIndex].VertexColors[i].Data);
             }
-        } else if (InnerChunkName == "MATERIALS") {
+        }
+        else if (InnerChunkName == "MATERIALS")
+        {
             LODs[LODIndex].Materials.SetNum(InnerArraySize);
-            for (auto i = 0; i < InnerArraySize; i++) {
-                LODs[LODIndex].Materials[i].MatIndex = i;
+            for (auto i = 0; i < InnerArraySize; i++)
+            {
                 LODs[LODIndex].Materials[i].Name = ReadBufferFString(Buffer, InnerOffset);
+                LODs[LODIndex].Materials[i].Path = ReadBufferFString(Buffer, InnerOffset);
                 LODs[LODIndex].Materials[i].FirstIndex = ReadBufferData<int32>(Buffer, InnerOffset);
                 LODs[LODIndex].Materials[i].NumFaces = ReadBufferData<int32>(Buffer, InnerOffset);
             }
-        } else if (InnerChunkName == "TEXCOORDS") {
+        }
+        else if (InnerChunkName == "TEXCOORDS")
+        {
             LODs[LODIndex].TextureCoordinates.SetNum(InnerArraySize);
-            for (auto i = 0; i < InnerArraySize; i++) {
+            for (auto i = 0; i < InnerArraySize; i++)
+                {
                 int32 UVCount = ReadBufferData<int32>(Buffer, InnerOffset);
                 LODs[LODIndex].TextureCoordinates[i].SetNum(UVCount);
-                for (auto j = 0; j < UVCount; j++) {
+                for (auto j = 0; j < UVCount; j++)
+                {
                     float U = ReadBufferData<float>(Buffer, InnerOffset);
                     float V = ReadBufferData<float>(Buffer, InnerOffset);
-                    LODs[LODIndex].TextureCoordinates[i][j] = FVector2f(U, 1 - V);
+                    LODs[LODIndex].TextureCoordinates[i][j] = FVector2f(U,V);
                 }
             }
-        } else if (InnerChunkName == "SOCKETS") {
+        }
+        else if (InnerChunkName == "SOCKETS")
+        {
             Skeleton.Sockets.SetNum(InnerArraySize);
-            for (auto i = 0; i < InnerArraySize; i++) {
+            for (auto i = 0; i < InnerArraySize; i++)
+            {
                 Skeleton.Sockets[i].SocketName = ReadBufferFString(Buffer, InnerOffset);
                 Skeleton.Sockets[i].SocketParentName = ReadBufferFString(Buffer, InnerOffset);
                 Skeleton.Sockets[i].SocketPos = ReadBufferData<FVector3f>(Buffer, InnerOffset);
                 Skeleton.Sockets[i].SocketRot = ReadBufferQuat(Buffer, InnerOffset);
                 Skeleton.Sockets[i].SocketScale = ReadBufferData<FVector3f>(Buffer, InnerOffset);
             }
-        } else if (InnerChunkName == "BONES") {
+        }
+        else if (InnerChunkName == "BONES")
+        {
             Skeleton.Bones.SetNum(InnerArraySize);
-            for (auto i = 0; i < InnerArraySize; i++) {
+            for (auto i = 0; i < InnerArraySize; i++)
+            {
                 Skeleton.Bones[i].BoneName = ReadBufferFString(Buffer, InnerOffset);
                 Skeleton.Bones[i].BoneParentIndex = ReadBufferData<int32>(Buffer, InnerOffset);
                 Skeleton.Bones[i].BonePos = ReadBufferData<FVector3f>(Buffer, InnerOffset);
                 Skeleton.Bones[i].BoneRot = ReadBufferQuat(Buffer, InnerOffset);
             }
-        } else if (InnerChunkName == "WEIGHTS") {
+        }
+        else if (InnerChunkName == "WEIGHTS")
+        {
             LODs[LODIndex].Weights.SetNum(InnerArraySize);
-            for (auto i = 0; i < InnerArraySize; i++) {
+            for (auto i = 0; i < InnerArraySize; i++)
+            {
                 LODs[LODIndex].Weights[i].WeightBoneIndex = ReadBufferData<short>(Buffer, InnerOffset);
                 LODs[LODIndex].Weights[i].WeightVertexIndex = ReadBufferData<int32>(Buffer, InnerOffset);
                 LODs[LODIndex].Weights[i].WeightAmount = ReadBufferData<float>(Buffer, InnerOffset);
             }
-        } else if (InnerChunkName == "MORPHTARGETS") {
+        }
+        else if (InnerChunkName == "MORPHTARGETS")
+        {
             LODs[LODIndex].Morphs.SetNum(InnerArraySize);
-            for (auto i = 0; i < InnerArraySize; i++) {
+            for (auto i = 0; i < InnerArraySize; i++)
+            {
                 LODs[LODIndex].Morphs[i].MorphName = ReadBufferFString(Buffer, InnerOffset);
                 const auto DeltaNum = ReadBufferData<int32>(Buffer, InnerOffset);
                 LODs[LODIndex].Morphs[i].MorphDeltas.SetNum(DeltaNum);
-                for (auto j = 0; j < DeltaNum; j++) {
+                for (auto j = 0; j < DeltaNum; j++)
+                {
                     LODs[LODIndex].Morphs[i].MorphDeltas[j].MorphPosition = ReadBufferData<FVector3f>(Buffer, InnerOffset);
                     LODs[LODIndex].Morphs[i].MorphDeltas[j].MorphNormals = ReadBufferData<FVector3f>(Buffer, InnerOffset);
                     LODs[LODIndex].Morphs[i].MorphDeltas[j].MorphVertexIndex = ReadBufferData<int32>(Buffer, InnerOffset);
                 }
             }
-        } else {
-            InnerOffset += InnerByteSize;
         }
+        else if (InnerChunkName == "VIRTUALBONES")
+        {
+            Skeleton.VirtualBones.SetNum(InnerArraySize);
+            for (auto i = 0; i < InnerArraySize; i++)
+            {
+                Skeleton.VirtualBones[i].SourceBoneName = ReadBufferFString(Buffer, InnerOffset);
+                Skeleton.VirtualBones[i].TargetBoneName = ReadBufferFString(Buffer, InnerOffset);
+                Skeleton.VirtualBones[i].VirtualBoneName = ReadBufferFString(Buffer, InnerOffset);
+            }
+        }
+        else if (InnerChunkName == "METADATA")
+        {
+            Skeleton.Path = ReadBufferFString(Buffer, InnerOffset);
+        }
+        else
+            InnerOffset += InnerByteSize;
     }
     Offset += ByteSize;
 }
