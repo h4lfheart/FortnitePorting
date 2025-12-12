9d7423cfaae43f666fc3218b6f297a0d41d6c55c
halfuwu <maxman1346@gmail.com>
2025-12-12 13:07:34 -0500

    fixes for portle download, material preview, and prefabs

--- FortnitePorting/Models/Material/MaterialData.cs
+++ FortnitePorting/Models/Material/MaterialData.cs
@@ -507,8 +507,8 @@ public partial class MaterialData : ObservableObject
                     "MaterialExpressionNamedRerouteUsage" => expression.Get<UMaterialExpression>("Declaration"),
                     _ => expression
                 };
-                
-                var name = declaration.Get<FName>("Name").Text;
+
+                var name = declaration.GetOrDefault<FName?>("Name")?.Text ?? "Unnamed";
                 var nodeColor = declaration.GetOrDefault<FLinearColor>("NodeColor");
                 nodeColor.R *= 0.25f;
                 nodeColor.G *= 0.25f;
@@ -585,7 +585,7 @@ public partial class MaterialData : ObservableObject
                         constantColor.A = 1;
                         break;
                     case "MaterialExpressionVectorParameter":
-                        node.Label = expression.Get<FName>("ParameterName").Text;
+                        node.Label = expression.GetOrDefault<FName?>("ParameterName")?.Text ?? expression.Name;;
                         break;
                 }
                 
@@ -643,7 +643,7 @@ public partial class MaterialData : ObservableObject
             
             case "MaterialExpressionScalarParameter":
             {
-                var name = expression.Get<FName>("ParameterName").Text;
+                var name = expression.GetOrDefault<FName?>("ParameterName")?.Text ?? expression.Name;
                 var sliderMin = expression.GetOrDefault<float>("SliderMin", 0);
                 var sliderMax = expression.GetOrDefault<float>("SliderMax", 1);
                 var defaultValue = expression.GetOrDefault<float>("DefaultValue");
