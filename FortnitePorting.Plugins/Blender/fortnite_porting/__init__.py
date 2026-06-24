0f0524fff7783295fdfe495c7b5321c617d06e75
halfuwu <maxman1346@gmail.com>
2026-06-23 21:14:47 -0400

    build: bump to v4.1.8

--- FortnitePorting.Plugins/Blender/fortnite_porting/__init__.py
+++ FortnitePorting.Plugins/Blender/fortnite_porting/__init__.py
@@ -20,7 +20,7 @@ bl_info = {
     "description": "Import Server for Fortnite Porting",
     "author": "Half",
     "blender": (5, 0, 0),
-    "version": (4, 1, 7),
+    "version": (4, 1, 8),
     "category": "Import-Export",
 }
 
--- FortnitePorting/Globals.cs
+++ FortnitePorting/Globals.cs
@@ -9,7 +9,7 @@ namespace FortnitePorting;
 public static class Globals
 {
     public static string VersionString => Version.GetDisplayString();
-    public static readonly FPVersion Version = new(4, 1, 7);
+    public static readonly FPVersion Version = new(4, 1, 8);
     public const string ApplicationTag = "FortnitePorting";
     
     public static readonly FilePickerFileType MappingsFileType = new("Unreal Mappings") { Patterns = [ "*.usmap" ] };
