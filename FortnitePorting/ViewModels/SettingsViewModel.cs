3a157719892860b59f67afb32bba49a7a05a3eec
halfuwu <maxman1346@gmail.com>
2026-07-19 16:25:25 -0400

    feat(settings): clear cache utility

--- FortnitePorting/ViewModels/SettingsViewModel.cs
+++ FortnitePorting/ViewModels/SettingsViewModel.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -22,6 +25,28 @@ public partial class SettingsViewModel(SettingsService settings, SupabaseService
     {
         App.Launch(App.ApplicationDataFolder.FullName);
     }
+
+    [RelayCommand]
+    public void ClearCache()
+    {
+        const string messageId = "ClearCache";
+        Info.Message("Clear Cache", "Clearing cached files...", id: messageId, autoClose: false);
+
+        TaskService.Run(() =>
+        {
+            var (cleared, skipped) = ClearFolder(UEParse.CacheFolder);
+            var dataResult = ClearFolder(App.DataFolder);
+            cleared += dataResult.Cleared;
+            skipped += dataResult.Skipped;
+
+            Info.CloseMessage(messageId);
+
+            var message = skipped > 0
+                ? $"Cleared {cleared} files ({skipped} skipped due to access errors)."
+                : $"Successfully cleared {cleared} files.";
+            Info.Message("Clear Cache", message);
+        });
+    }
     
     [RelayCommand]
     public async void Save()
@@ -41,5 +66,43 @@ public partial class SettingsViewModel(SettingsService settings, SupabaseService
     {
         App.RestartWithMessage("Restart Application", "Are you sure you would like to restart?");
     }
-    
+
+    private static (int Cleared, int Skipped) ClearFolder(DirectoryInfo folder)
+    {
+        if (!folder.Exists) 
+            return (0, 0);
+
+        var cleared = 0;
+        var skipped = 0;
+
+        foreach (var file in folder.EnumerateFiles("*", SearchOption.AllDirectories))
+        {
+            try
+            {
+                file.IsReadOnly = false;
+                file.Delete();
+                cleared++;
+            }
+            catch (Exception e) when (e is IOException or UnauthorizedAccessException)
+            {
+                skipped++;
+            }
+        }
+
+        foreach (var directory in folder.EnumerateDirectories("*", SearchOption.AllDirectories)
+                     .OrderByDescending(directory => directory.FullName.Length))
+        {
+            try
+            {
+                if (!directory.EnumerateFileSystemInfos().Any())
+                    directory.Delete();
+            }
+            catch (Exception e) when (e is IOException or UnauthorizedAccessException)
+            {
+            }
+        }
+
+        folder.Create();
+        return (cleared, skipped);
+    }
 }
