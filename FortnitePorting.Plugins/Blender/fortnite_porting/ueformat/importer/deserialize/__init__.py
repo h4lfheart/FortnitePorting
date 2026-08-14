874fac8555171aa7c6752c99b4a4dd41355cfaf3
halfuwu <maxman1346@gmail.com>
2026-08-14 14:14:02 -0400

    build: use UEFormat submodule

--- FortnitePorting.Plugins/Blender/fortnite_porting/ueformat/importer/deserialize/__init__.py
+++
@@ -1,3 +0,0 @@
-from .anim import read_anim
-from .model import read_model
-from .pose import read_pose
--- FortnitePorting.Plugins/Unreal/UEFormat/Source/UEFormat/Private/Widgets/Anim/UEFAnimImportOptions.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright © 2025 Marcel K. All rights reserved.
-
-#include "Widgets/Anim/UEFAnimImportOptions.h"
-
-UEFAnimImportOptions::UEFAnimImportOptions()
-{
-	Skeleton = nullptr;
-}
